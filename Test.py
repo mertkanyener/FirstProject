dcc622e9638f8184fca38e3a0eafec5d3736e878
mertkanyener <mertkanyener@outlook.com>
2017-01-29 22:29:22 +0300

    saim çalışma

--- Test.py
+++ Test.py
@@ -1,2 +1,21 @@
+name = input("Please enter your name: ")
+age = input("Age:")
+year = (100 - int(age)) + 2017
+print("your name is " + name + " your age is " + age + " you will be 100 years old in " + str(year))
 
+number = int(input("give number"))
+if number%2 == 0:
+    print("bu çift hacı")
+    if number%4==0:
+        print("4ün katı")
+else:
+    print("tek sayı")
+
+a = [3, 6, 90, 0, 4, 45, 7, 2]
+yarrak=[]
+number = int(input("number gir yarramın bası"))
+for x in a:
+    if x<number:
+        yarrak.append(x)
+print(yarrak)
 
